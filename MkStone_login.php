6dc6051cf8893c6292ae36ff865cbba89edb6145
shijingyu <shijy675497282@163.com>
2016-09-29 22:47:02 +0800

    Update MkStone_login.php

--- MkStone_login.php
+++ MkStone_login.php
@@ -1,38 +1,13 @@
-<?PHP
-    header("Content-Type: text/html; charset=utf8");
-    if(!isset($_POST["submit"])){
-        exit("错误执行");
-    }//检测是否有submit操作 
-
-
-    include('MkStone_Connect.php');//链接数据库
-    $name = $_POST['username'];//post获得用户名表单值
-    $passowrd = $_POST['password'];//post获得用户密码单值
-
-    if ($name && $passowrd){//如果用户名和密码都不为空
-             $sql = "select * from user where usename = '$name' and password='$passowrd'";//检测数据库是否有对应的username和password的sql
-             $result = mysql_query($sql);//执行sql
-             $rows=mysql_num_rows($result);//返回一个数值
-             if($rows){//0 false 1 true
-                   header("refresh:0;url=index.html");//如果成功跳转至index.html页面
-                   exit;
-             }else{
-            
+echo "用户名或密码错误";
                 echo "
                     <script>
-                    alert('登陆失败!!');location.href='login_register.html'</script>   
-                    
+                            setTimeout(function(){window.location.href='login.html';},1000);
+                    </script>
 
-                ";
-             }
-             
+                ";//如果错误使用js 1秒后跳转到登录页面重试; echo "表单填写不完整";
+                echo "
+                      <script>
+                            setTimeout(function(){window.location.href='login.html';},1000);
+                      </script>";
 
-    }else{
-                echo "<script>alert('用户名或者密码没有填写!!');location.href='login_register.html'</script>;"
-           <     
-
-                          
-   }
-
-    mysql_close();//关闭数据库
-?>
+                        //如果错误使用js 1秒后跳转到登录页面重试;login_register.htmllogin_register.halert('注册成功');location.href='login_register.html'</script用户名或者密码错alert('注册成功');location.href='login_register.html'</scri用户名或者密码为cd ..rm -rf mk
