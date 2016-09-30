d4fc4135d13f16d5672040afeca5668d71f6872f
shijingyu <shijy675497282@163.com>
2016-09-30 11:22:37 +0800

    Create MkStone_Connect.php

---
+++ MkStone_Connect.php
@@ -0,0 +1,3 @@
+git add git add git add git commit -m "前端添加注册验证cd ..cd ~/docgigit add git add clone https://github.com/soFlyMan/mkStone.ggit clone https://github.com/soFlyMan/mkStone.gitgitMkStone_Connect.php
+MkStone_login.php
+MkStone_Signup.php add git add git aadd git commit "同步git commit -m "1.0.2git push origin mastergit branch sjy-mkStonegit checkout sjgitstatusgit add git add MkStone_Connect.phpgit add MkStone_login.phpgit add MkStone_Signup.phpgit commit -m ""sssgit push origin sjy"""
